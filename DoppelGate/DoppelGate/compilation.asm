8150abddebd05394fc9bd4cbb24edb8ead5317dd
asaurusrex <58984065+asaurusrex@users.noreply.github.com>
2022-03-23 14:35:53 -0400

    Update compilation.asm
    Updated .asm file to implement random jumps to unhooked function syscall instructions inside in-mem ntdll. inspired by syswhispers3

--- DoppelGate/DoppelGate/compilation.asm
+++ DoppelGate/DoppelGate/compilation.asm
@@ -2,19 +2,31 @@
 	wSystemCall DWORD 000h 
  
 .code    
-	DoppelGate PROC        
-		mov wSystemCall, 000h         
-		mov wSystemCall, ecx        
-		ret     
-	DoppelGate ENDP 
- 
-    DoppelDescent PROC        
-		mov r10, rcx         
-		mov eax, wSystemCall 
-        syscall         
-		ret
-	DoppelDescent ENDP 
 
-	
+EXTERN Fetch_Random_Sys: PROC
+
+DoppelGate PROC        
+	mov wSystemCall, 000h         
+	mov wSystemCall, ecx        
+	ret     
+DoppelGate ENDP 
+ 
+DoppelDescent PROC
+	mov [rsp +8], rcx          ; Save registers.
+	mov [rsp+16], rdx
+	mov [rsp+24], r8
+	mov [rsp+32], r9
+	sub rsp, 28h
+	call Fetch_Random_Sys	;grab syscall offset from random unhooked api
+	mov r12, rax                           ; Save the address of the syscall
+	add rsp, 28h
+	mov rcx, [rsp+8]                      ; Restore registers.
+	mov rdx, [rsp+16]
+	mov r8, [rsp+24]
+	mov r9, [rsp+32]
+	mov r10, rcx
+	mov eax, wSystemCall
+	jmp r12                                ; Jump to -> Invoke system call.
+DoppelDescent ENDP 
 
 end 
